6567ae1996943e9e8c1ab9bfd095dc76a9ba16e2
MinjoonHK <minjoon.park.hk@gmail.com>
2026-02-14 11:47:23 +0900

    🐛 FIX: Suspense 추가

--- apps/web/app/archiver/home/page.tsx
+++ apps/web/app/archiver/home/page.tsx
@@ -1,1 +1,13 @@
-export { ArchiverHomePage as default, metadata } from '@/pages/archiver/home';
+import { Suspense } from 'react';
+
+import { ArchiverHomePage, metadata } from '@/pages/archiver/home';
+
+export { metadata };
+
+export default function Page(): React.ReactElement {
+  return (
+    <Suspense fallback={null}>
+      <ArchiverHomePage />
+    </Suspense>
+  );
+}
--- apps/web/app/mypage/page.tsx
+++ apps/web/app/mypage/page.tsx
@@ -1,7 +1,13 @@
+import { Suspense } from 'react';
+
 import { MyPage, metadata } from '@/pages/mypage';
 
 export { metadata };
 
 export default function MyPageRoute(): React.ReactNode {
-  return <MyPage />;
+  return (
+    <Suspense fallback={null}>
+      <MyPage />
+    </Suspense>
+  );
 }
